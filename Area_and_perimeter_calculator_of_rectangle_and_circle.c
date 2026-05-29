58769b020bb77d627e847b7de9b9d5ec45909d1a
sarthakjaras-0108 <sarthakjaras1972@gmail.com>
2026-05-29 19:57:36 +0530

    Add area and perimeter calculator for rectangle and circle

--- Area_and_perimeter_calculator_of_rectangle_and_circle.c
+++ Area_and_perimeter_calculator_of_rectangle_and_circle.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#define PI 3.14159
 
+int main() {
+    float length, breadth, radius, rect_area, rect_perimeter, circle_area, circle_circumference;
+    
+    printf("Enter the length of the rectangle: ");
+    scanf("%f", &length);
+    printf("\nEnter the breadth of the rectangle: ");
+    scanf("%f", &breadth);
+
+    printf("\nEnter the radius of the circle: ");
+    scanf("%f", &radius);
+
+    rect_area = length * breadth;
+    rect_perimeter = 2 * (length + breadth);
+
+    circle_area = PI * radius * radius;
+    circle_circumference = 2 * PI * radius;
+
+    printf("\n--- Rectangle Results ---");
+    printf("\nArea: %.2f", rect_area);
+    printf("\nPerimeter: %.2f", rect_perimeter);
+
+    printf("\n\n--- Circle Results ---");
+    printf("\nArea: %.2f", circle_area);
+    printf("\nCircumference: %.2f\n", circle_circumference);
+
+    return 0;
+}
