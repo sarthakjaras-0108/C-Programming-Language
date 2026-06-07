e7e768235a2ac0bd2c119048d79539458fd289ed
sarthakjaras-0108 <sarthakjaras1972@gmail.com>
2026-06-07 18:49:02 +0530

    Program to print student's grade by accepting percent marks using switch statement.

---
+++ program_to_get_grade_using_student_percentage_using_switch_statements.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+    int marks;
+    scanf("%d", &marks);
+        switch (marks / 10) {
+        case 10:
+        case 9:
+            printf("Grade: A\n");
+            break;
+        case 8:
+            printf("Grade: B\n");
+            break;
+        case 7:
+            printf("Grade: C\n");
+            break;
+        case 6:
+            printf("Grade: D\n");
+            break;
+        case 5:
+            printf("Grade: E\n");
+            break;
+        default:
+            printf("Grade: F\n");
+            break;
+    }
+    return 0;
+}
